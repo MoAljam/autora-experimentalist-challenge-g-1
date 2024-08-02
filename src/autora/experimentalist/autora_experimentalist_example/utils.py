00ed9d065f902e1375d3754fab6f483289e67bf6
rmathew-uni <rmathew@uni-osnabrueck.de>
2024-08-02 14:53:07 +0200

    created function for scorer

--- src/autora/experimentalist/autora_experimentalist_example/utils.py
+++ src/autora/experimentalist/autora_experimentalist_example/utils.py
@@ -1,1 +1,14 @@
+from scipy.special import rel_entr
+def divergence_scorer(conditions: Union[pd.DataFrame, np.ndarray],
+                             models: List,
+                             num_samples: Optional[int] = None,
+                             ) -> float:
+    
+    # condtion_pool = conditions.copy()
+    model_new = models[-1]
+    model_old = models[-2]
+    score = mean(sum(rel_entr(model_new.predict(conditions), model_old.predict(conditions))))
+    
+    print("score:", score)
 
+    return score
